9bc51c3a38b3d26c0d4ff14bde3ad110670495fe
Charles <ckelly218@gmail.com>
2022-08-16 22:10:47 -0700

    Modified a few code on separate file. Main issue was js loading before html. Used defer

--- javascript.js
+++ javascript.js
@@ -1,48 +1,62 @@
+const buttons = document.querySelectorAll('button');
 let playerScore = 0;
 let computerScore = 0;
 let totalGames = 0;
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () =>{
+        playRound(button.id);
+    });
+});
+
+function disableButtons(){
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })}
 
 function computerPlay(){
 	let choices = ['rock','paper','scissors'];
 	return choices[Math.floor(Math.random() * choices.length)]
     }
-    
-function playGame(){
-	let choices = ['rock','paper','scissors'];
-    let rock = choices.indexOf('rock');
-    let paper = choices.indexOf('paper');
-    let scissors = choices.indexOf('scissors');
-    let text = ""; 
+
+function playRound(userInput){
     let computerChoice = computerPlay();
-    const userInput = prompt('Choose rock, paper, or scissors').toLowerCase()
-	
-    
-  
+    text = ''
+
     if ((userInput == 'rock' && computerChoice == 'scissors') ||
         (userInput == 'paper' && computerChoice == 'rock') ||
-        (userInput == 'scissors' && computerChoice == 'paper')){
+        (userInput == 'scissors' && computerChoice == 'paper')) {
 
         playerScore += 1
         totalGames += 1
-        text = ("You win! You chose " + `${userInput}.` + `The computer chose ` + `${computerChoice}.`);
+        alert("You win! You chose " + `${userInput}.` + `The computer chose ` + `${computerChoice}.`);
+
+        if (playerScore == 5) {
+            alert('You win the game')
+            disableButtons();
+        }
     }
     else if (userInput == computerChoice){
 
         totalGames +=1
-        text = ("It's a tie, you both chose " + `${userInput}.`)
+        alert("It's a tie, you both chose " + `${userInput}.`)
     }
-    else{
+    else {
         computerScore += 1
         totalGames += 1
-        text = ('You lose, the computer chose ' + `${computerChoice}` + ' and you chose ' + `${userInput}.`);
-        } 
+        alert('You lose, the computer chose ' + `${computerChoice}` + ' and you chose ' + `${userInput}.`);
+        
+        if (computerScore == 5) {
+            alert('You lost. Computer won. Try again.')
+            disableButtons();
+        }
+    }
+    document.getElementById('playerScore').textContent = playerScore;
+    document.getElementById('computerScore').textContent = computerScore;
     
-
-    
-    document.getElementById('demo').innerHTML = text;
-    document.getElementById('playerscore').innerHTML = playerScore
-    document.getElementById('pc').innerHTML = computerScore;
-    document.getElementById('totalgames').innerHTML = totalGames
-
 }
+
+
+
+
+
